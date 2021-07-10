33864a206bf15514ca118c325fb5c5806cc74e9a
ASUS <Yehor Kholodiuk>
2021-07-10 18:39:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
-
-function App() {
+import Counter2 from "./Counter2";
+export default function App()
+ {
+   const [counter, setCounter] = useState(1);
+   const plusMinus = (number) => {
+     setCounter(counter + number)
+   }
+   const [list,setList] = useState([1,2,3])
+   const [open,setOpen] = useState(false)
+   const toggle = () => {
+     setOpen(!open)
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1> Use Effect</h1>
+        <button onClick={() => plusMinus(-1)}> -1</button>
+        {counter}
+        <button onClick={() => plusMinus(+1)}> +1</button>
+        <hr/>
+        <button onClick={toggle} > open counter 2</button>
+        {open && <Counter2/>}
+        <hr/>
     </div>
   );
 }
 
-export default App;
+
+
